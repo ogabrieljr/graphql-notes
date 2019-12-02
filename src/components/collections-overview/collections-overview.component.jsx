409a1f67d2a09f96a4482300a20ae4ec5d2722b3
Gabriel <jgj_junior@yahoo.com.br>
2019-12-02 17:22:18 -0200

    added comments, removed logger and format

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import CollectionPreview from '../collection-preview/collection-preview.component';
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
-import './collections-overview.styles.scss';
+import "./collections-overview.styles.scss";
 
 const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
+  // Array(5) [ {…}, {…}, {…}, {…}, {…} ]
+  <div className="collections-overview">
     {collections.map(({ id, ...otherCollectionProps }) => (
+      // Object { title: "Hats", routeName: "hats", items: (9) […] }
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
   </div>
--- src/index.js
+++ src/index.js
@@ -25,18 +25,18 @@ const client = new ApolloClient({
   cache
 });
 
-client
-  .query({
-    query: gql`
-      {
-        getCollectionsByTitle(title: "hats") {
-          id
-          title
-        }
-      }
-    `
-  })
-  .then(res => console.log(res));
+// client
+//   .query({
+//     query: gql`
+//       {
+//         getCollectionsByTitle(title: "hats") {
+//           id
+//           title
+//         }
+//       }
+//     `
+//   })
+//   .then(res => console.log(res));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
