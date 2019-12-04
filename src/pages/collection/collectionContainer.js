7594d28818930df6093f840470519693396e56ed
Gabriel <jgj_junior@yahoo.com.br>
2019-12-04 18:59:37 -0200

    using props render

--- src/pages/collection/collectionContainer.js
+++ src/pages/collection/collectionContainer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import Spinner from "../../components/spinner/spinner.component";
-import { useQuery } from "@apollo/react-hooks";
 import CollectionItem from "../../components/collection-item/collection-item.component";
+import { Query } from "react-apollo";
 
 const GET_COLLECTION_BY_TITLE = gql`
   query getCollectionsByTitle($title: String!) {
@@ -20,24 +20,26 @@ const GET_COLLECTION_BY_TITLE = gql`
 `;
 
 export default function CollectionContainer({ match }) {
-  const { loading, error, data } = useQuery(GET_COLLECTION_BY_TITLE, {
-    variables: { title: match.params.collectionId }
-  });
-
-  if (loading) return <Spinner />;
-  if (error) return `Error! ${error}`;
-
-  console.log(data.getCollectionsByTitle.items);
-  const { title, items } = data.getCollectionsByTitle;
-
   return (
-    <div>
-      <h2 className="title">{title}</h2>
-      <div className="items">
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
+    <Query
+      query={GET_COLLECTION_BY_TITLE}
+      variables={{ title: match.params.collectionId }}>
+      {({ loading, error, data }) => {
+        if (loading) return <Spinner />;
+        if (error) return <p>Error</p>;
+        const { title, items } = data.getCollectionsByTitle;
+        return (
+          <div>
+            <h2 className="title">{title}</h2>
+            <div className="items">
+              {items.map(item => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+            ;
+          </div>
+        );
+      }}
+    </Query>
   );
 }
